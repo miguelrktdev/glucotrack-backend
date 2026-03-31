97567c4f574ea864308f15230e0be72d7ed26f44
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-03-31 15:44:24 -0300

    ✨ Feat: Create global dotenv validation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "@/app.ts";
+import { env } from "@/config/env.ts";
 
 app
   .listen({
-    port: 3333,
-    host: "0.0.0.0",
+    port: env.PORT,
+    host: env.HOST,
   })
   .then(() => {
     console.log("HTTP Server Running 🚀");
