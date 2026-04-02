18bb8a86bbbab291a697f1c49a9260fccc420e86
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-04-02 07:09:53 -0300

    ✨ Feat: Create global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
-import { fastify } from "fastify"
+import { fastify } from "fastify";
+import { StatusCodes } from "http-status-codes";
+import { ZodError } from "zod";
 
-export const app = fastify()
+export const app = fastify();
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: error.message,
+    });
+  }
+
+  return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+    success: false,
+    message: "INTERNAL SERVER ERROR",
+  });
+});
